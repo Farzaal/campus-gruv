6e3f5b4ec1784570b4f38a60bf2abd1190c3d2a2
farzal <farzalkhan786@gmail.com>
2019-12-13 19:22:36 +0500

    cloudinary creds changed

--- config/cloudinary.js
+++ config/cloudinary.js
@@ -1,7 +1,7 @@
 const Env = use('Env')
  
 module.exports = {
-  name: Env.get('CLOUDINARY_NAME', 'dp8ybac11'),
-  api_key: Env.get('CLOUDINARY_API_KEY', '691494556986484'),
-  api_secret: Env.get('CLOUDINARY_API_SECRET', 'MmBILQnYvJATLA_QyZa97uLk2s0')
+  name: Env.get('CLOUDINARY_NAME', 'drtvwanfx'),
+  api_key: Env.get('CLOUDINARY_API_KEY', '111264895227789'),
+  api_secret: Env.get('CLOUDINARY_API_SECRET', 'XKwfoJIvkQyq40b1SxsjAShJ0dI')
 }
