bcca637b77f6e4c4b85e02c0f4667ddf1681d90d
farzal <farzalkhan786@gmail.com>
2019-12-15 16:53:58 +0500

    removing key files

--- config/cloudinary.js
+++
@@ -1,7 +0,0 @@
-const Env = use('Env')
- 
-module.exports = {
-  name: Env.get('CLOUDINARY_NAME', 'drtvwanfx'),
-  api_key: Env.get('CLOUDINARY_API_KEY', '111264895227789'),
-  api_secret: Env.get('CLOUDINARY_API_SECRET', 'XKwfoJIvkQyq40b1SxsjAShJ0dI')
-}
--- config/sendGrid.js
+++
@@ -1,5 +0,0 @@
-const sgMail = require('@sendgrid/mail');
-sendGridApiKey = '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'
-sgMail.setApiKey(sendGridApiKey)
-
-module.exports = { sgMail }
