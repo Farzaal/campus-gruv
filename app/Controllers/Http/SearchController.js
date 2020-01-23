6963d85e4f3b8755956841ffcc847097a382ba22
Farzaal <farzalkhan786@gmail.com>
2020-01-24 04:27:29 +0500

    search post by user id and description

--- app/Controllers/Http/SearchController.js
+++ app/Controllers/Http/SearchController.js
@@ -63,8 +63,23 @@ class SearchController {
         const body = request.get()
         const { type, page } = body
         const { campus_id } = await this.getFromAuthUser(auth)
-        if(R.equals(type, 'post') && body.user_id) {
+        if(R.equals(type, 'post') && body.user_id && !body.description) {
             const posts = await PostMaster.query().active().where('user_id', body.user_id)
+            .with('postDetail', (builder) => builder.select('post_detail_title', 'image_url'))
+            .with('comments.user', (builder) => builder.select('id', 'first_name', 'last_name', 'email', 'profile_pic_url'))
+            .with('users', (builder) => builder.select('id', 'first_name', 'last_name', 'email', 'profile_pic_url'))
+            .with('users.userFollower')
+            .with('userSavedPost.post', (builder) => builder.select('id'))
+            .with('userWiseLike.user', (builder) => builder.select('id'))
+            .with('postCategory')
+            .with('campuses', (builder) => builder.select('id', 'description'))
+            .orderBy('created_at', 'DESC').paginate(page)
+            const postsJson = posts.toJSON()
+            return response.status(200).json(postsJson) 
+        }
+        if(R.equals(type, 'post') && body.description && body.user_id) {
+            console.log("asdasd")
+            const posts = await PostMaster.query().active().where('user_id', body.user_id).where('description', 'LIKE', `%${body.description}%`)
             .with('postDetail', (builder) => builder.select('post_detail_title', 'image_url'))
             .with('comments.user', (builder) => builder.select('id', 'first_name', 'last_name', 'email', 'profile_pic_url'))
             .with('users', (builder) => builder.select('id', 'first_name', 'last_name', 'email', 'profile_pic_url'))
